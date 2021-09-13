c98d4738bb578ba9b17fbff8314ff36e7e1a66ab
maxhofmanf <99068281@mydavinci.nl>
2021-09-13 09:26:00 +0100

    forever-young

---
+++ klok.py
@@ -0,0 +1,4 @@
+for i in range (1, 12):
+    print (i , ("AM"))
+for i in range (1, 12):
+    print (i , ("pm"))
---
+++ rocket.py
@@ -0,0 +1,3 @@
+for number in range(30):
+    print(30 - number)
+print("rocket has launched")
